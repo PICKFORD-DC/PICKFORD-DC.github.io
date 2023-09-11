2c027120cbe3a017235cbfed58dcdf6a0e440e59
Agus Maulana <105628212+PICKFORD-DC@users.noreply.github.com>
2023-09-12 06:49:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var audio = document.getElementById("myAudio");
+
+  function playMusic() {
+    audio.play();
+  }
+
+  function pauseMusic() {
+    audio.pause();
+  }
+  function wa() {
+    alert('Kamu Akan Di Arahkan Ke Wa Guweh')
+    window.open('https://api.whatsapp.com/send?phone=6283806211925&text=Halo Agus Gemooy ><','_blank')
+  }
+  function ig() {
+    alert('Kamu akan di Arahkan Ke Instagram Guweh')
+    window.open('https://instagram.com/pick_ford867?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D','_blank')
+  }
